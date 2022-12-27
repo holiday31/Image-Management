7b58207ca5f8c26e1bbb790ebc9cacac52852bfe
jimin <holiday31@naver.com>
2022-12-27 23:53:08 +0900

    init

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const multer = require("multer");
+const { v4 : uuid} = require("uuid");
+const mime =  require("mime-types");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "./uploads"),
+    filename: (req, file, cb) => 
+        cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+  });
+const upload = multer({ storage });
+
+const app= express();
+const PORT = 5000;
+
+app.use("/uploads",express.static("uploads"));
+
+
+app.post("/upload",upload.single("imageTest"), (req,res)=>{
+    console.log(req.file);
+    res.json(req.file);
+});
+
+app.listen(PORT, ()=> console.log("Express server listening on PORT "+PORT));
